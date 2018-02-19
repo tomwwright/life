9d48a8be16dad748b4ff99a73cef8513d8df058f
Tom Wright <tom.w.wright@live.com>
2018-02-19 21:03:19 +1100

    Add ability to export

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 // next.config.js
 const withTypescript = require('@zeit/next-typescript')
 module.exports = withTypescript()
+
+module.exports.exportPathMap = function() {
+  return {
+    '/': { page: '/' }
+  }
+};
